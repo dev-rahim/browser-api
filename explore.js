081dc2de45160715c04be309982983c4cb3de1b9
dev-rahim <rahimuddin5915@gmail.com>
2022-09-21 10:58:10 +0600

    exploring browser api javascript document is a browser based object

---
+++ explore.js
@@ -0,0 +1,9 @@
+const name = 'Rahim';
+const student = {
+    name: 'rahim', roll: 127219, print: function (whatToPrint) {
+        console.log(whatToPrint);
+    }
+};
+student.print('hellow world');
+// similerly document is also a object but that's accable only for browsers
+document.getElementById('name');
